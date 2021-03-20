e66196b4c2eead264fd35e74af549487c685566e
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-03-20 00:41:37 -0300

    Layout ajustments

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -2,10 +2,15 @@ import './Main.css';
 import React from 'react';
 import Header from './Header'
 
-export default props =>
-    <React.Fragment>
-        <Header {...props} />
-            <main className="content">
-                Conteudo
-            </main>
-    </React.Fragment>
+export default (props) => (
+	<React.Fragment>
+		<Header {...props} />
+		<main className="content container-fluid">
+            <div className="p-3 mt3">
+
+
+            </div>
+
+        </main>
+	</React.Fragment>
+);
