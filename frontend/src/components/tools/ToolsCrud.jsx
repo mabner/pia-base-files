5eec4709fd0c6a96765ef53913c01a6f0c1b0532
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-03-21 00:11:29 -0300

    save, clear, get list methods for the crud

--- frontend/src/components/tools/ToolsCrud.jsx
+++ frontend/src/components/tools/ToolsCrud.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Main from '../template/Main';
+import axios from 'axios';
 
 const headerProps = {
 	icon: 'tools',
@@ -7,7 +8,42 @@ const headerProps = {
 	subtitle: 'Cadastro de ferramentas: Incluir, Listar, Alterar e Excluir.',
 };
 
+const baseUrl = 'http://localhost:3001/tools';
+const initialState = {
+	tools: { name: '', description: '', usage: '' },
+	list: [],
+};
+
 export default class ToolsCrud extends Component {
+	state = { ...initialState };
+
+	// Clears the form
+	clear() {
+		this.setState({ tools: initialState.tools });
+	}
+
+	// Saves new tools or alters existing one
+	save() {
+		const tools = this.state.tools;
+
+		// If tool has valid ID then PUT, no ID do a POST (new record)
+		const method = tools.id ? 'put' : 'post';
+
+		// PUT URL : POST URL
+		const url = tools.id ? `${baseUrl}/${tools.id}` : baseUrl;
+		axios[method](url, tools).then((resp) => {
+			// resp.data returns the data returned from the webservice (our json server)
+			const list = this.getUpdatedList(resp.data);
+			this.setState({ tools: initialState.tools, list });
+		});
+	}
+
+	getUpdatedList(tools) {
+		const list = this.state.list.filter((t) => t.id !== tools.id);
+		list.unshift(tools);
+		return list;
+	}
+
 	render() {
 		return <Main {...headerProps}>Cadastro de Ferramentas</Main>;
 	}
