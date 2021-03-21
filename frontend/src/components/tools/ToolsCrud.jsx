2b5e5000cf192e1c1c142675f29cfbeb6086adf1
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-03-21 03:53:27 -0300

    form for adding new tools to the list

--- frontend/src/components/tools/ToolsCrud.jsx
+++ frontend/src/components/tools/ToolsCrud.jsx
@@ -1,6 +1,6 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import Main from '../template/Main';
-import axios from 'axios';
 
 const headerProps = {
 	icon: 'tools',
@@ -51,13 +51,87 @@ export default class ToolsCrud extends Component {
 		this.setState({ tools });
 	}
 
-	renderForm() {}
+	renderForm ()
+	{
+		return (
+			<div className="form">
+				<div className="row">
+					<div className="col-12 col-md-6">
+						<div className="form-group">
+							<label>Nome</label>
+							<input
+								type="text"
+								className="form-control"
+								name="name"
+								value={this.state.tools.name}
+								onChange={(event) => this.updateField(event)}
+								placeholder="Digite o nome da ferramenta..."
+							/>
+						</div>
+					</div>
+					<div className="col-12 col-md-6">
+						<div className="form-group">
+							<label>Descrição</label>
+							<input
+								type="text"
+								className="form-control"
+								name="description"
+								value={this.state.tools.description}
+								onChange={(event) => this.updateField(event)}
+								placeholder="Digite uma descrição para a ferramenta..."
+							/>
+						</div>
+					</div>
+					<div className="col-12 col-md-6">
+						<div className="form-group">
+							<label>Tema</label>
+							<input
+								type="text"
+								className="form-control"
+								name="usage"
+								value={this.state.tools.usage}
+								onChange={(event) => this.updateField(event)}
+								placeholder="Digite o tema da ferramenta..."
+							/>
+						</div>
+					</div>
+				</div>
+				<hr />
+				<div className="row">
+					<div className="col-12 d-flex justify-content-end">
+						<button
+							className="btn btn-primary"
+							onClick={(event) => this.save(event)}
+						>
+							Salvar
+						</button>
 
-	renderTable() {}
+						<button
+							className="btn btn-secondary ml-2"
+							onClick={(event) => this.clear(event)}
+						>
+							Cancelar
+						</button>
+					</div>
+				</div>
+			</div>
+		);
 
-	renderRow() {}
+	}
+
+	renderTable ()
+	{
+
+	}
+
+	renderRow ()
+	{
+
+	}
 
 	render() {
-		return <Main {...headerProps}>Cadastro de Ferramentas</Main>;
+		return <Main { ...headerProps }>
+			{this.renderForm()}
+		</Main>;
 	}
 }
